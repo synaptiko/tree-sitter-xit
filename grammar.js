4b14c3baab73c2d911b5d8317b9a301a555f5a05
Jiří Prokop <jprokop@synaptiko.cz>
2022-04-09 18:06:17 +0200

    Add first tests for priorities and try to update the grammar

--- grammar.js
+++ grammar.js
@@ -1,32 +1,31 @@
 module.exports = grammar({
   name: 'xit',
 
-  extras: ($) => [],
+  extras: (_$) => [],
 
   conflicts: ($) => [[$.description]],
 
   rules: {
     document: ($) => repeat(choice($.task, '\n')),
-    task: ($) => choice(
-      $.open_task,
-      $.checked_task,
-      $.ongoing_task,
-      $.obsolete_task,
-    ),
+    task: ($) => choice($.open_task, $.checked_task, $.ongoing_task, $.obsolete_task),
     indent: ($) => seq(token(/[ ]{4}/), $.other_line),
-    main_line: ($) => token(/[^\n]+/),
-    other_line: ($) => token(/[^\n]+/),
-    open_checkbox: ($) => token(seq('[ ]')),
+    main_line: (_$) => token(/[^\n]+/),
+    other_line: (_$) => token(/[^\n]+/),
+    open_checkbox: (_$) => token(seq('[ ]')),
     open_task: ($) => seq($.open_checkbox, optional(seq(' ', optional($.description)))),
-    checked_checkbox: ($) => token(seq('[x]')),
+    checked_checkbox: (_$) => token(seq('[x]')),
     checked_task: ($) => seq($.checked_checkbox, optional(seq(' ', optional($.description)))),
-    ongoing_checkbox: ($) => token(seq('[@]')),
+    ongoing_checkbox: (_$) => token(seq('[@]')),
     ongoing_task: ($) => seq($.ongoing_checkbox, optional(seq(' ', optional($.description)))),
-    obsolete_checkbox: ($) => token(seq('[~]')),
+    obsolete_checkbox: (_$) => token(seq('[~]')),
     obsolete_task: ($) => seq($.obsolete_checkbox, optional(seq(' ', optional($.description)))),
-    description: ($) => choice(
-      $.main_line,
-      seq($.main_line, '\n', repeat(seq($.indent, '\n')), $.indent),
-    ),
+    description: ($) =>
+      choice(
+        seq($.priority, ' ', $.main_line),
+        seq($.priority, ' ', $.main_line, '\n', repeat(seq($.indent, '\n')), $.indent),
+        $.main_line,
+        seq($.main_line, '\n', repeat(seq($.indent, '\n')), $.indent)
+      ),
+    priority: (_$) => repeat1('!'),
   },
 });
