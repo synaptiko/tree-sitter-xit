0d86ee75e3e7110c528d0c7c188b01541299877e
Jiří Prokop <jprokop@synaptiko.cz>
2022-04-09 19:33:46 +0200

    Reformat grammar.js and add support for headlines

--- grammar.js
+++ grammar.js
@@ -1,32 +1,27 @@
 module.exports = grammar({
   name: 'xit',
 
-  extras: ($) => [],
+  extras: (_$) => [],
 
-  conflicts: ($) => [[$.description]],
+  conflicts: ($) => [[$.task, $.headline], [$.description]],
+  inline: ($) => [$.empty_line],
 
   rules: {
-    document: ($) => repeat(choice($.task, '\n')),
-    task: ($) => choice(
-      $.open_task,
-      $.checked_task,
-      $.ongoing_task,
-      $.obsolete_task,
-    ),
+    document: ($) => seq(repeat(choice($.headline, seq($.task, '\n'), $.empty_line)), optional($.task)),
+    headline: (_$) => seq(/[^\[ \n][^\n]+/, '\n'),
+    empty_line: (_$) => seq('\n', repeat(' ')),
+    task: ($) => choice($.open_task, $.checked_task, $.ongoing_task, $.obsolete_task),
     indent: ($) => seq(token(/[ ]{4}/), $.other_line),
-    main_line: ($) => token(/[^\n]+/),
-    other_line: ($) => token(/[^\n]+/),
-    open_checkbox: ($) => token(seq('[ ]')),
+    main_line: (_$) => token(/[^\n]+/),
+    other_line: (_$) => token(/[^\n]+/),
+    open_checkbox: (_$) => token(seq('[ ]')),
     open_task: ($) => seq($.open_checkbox, optional(seq(' ', optional($.description)))),
-    checked_checkbox: ($) => token(seq('[x]')),
+    checked_checkbox: (_$) => token(seq('[x]')),
     checked_task: ($) => seq($.checked_checkbox, optional(seq(' ', optional($.description)))),
-    ongoing_checkbox: ($) => token(seq('[@]')),
+    ongoing_checkbox: (_$) => token(seq('[@]')),
     ongoing_task: ($) => seq($.ongoing_checkbox, optional(seq(' ', optional($.description)))),
-    obsolete_checkbox: ($) => token(seq('[~]')),
+    obsolete_checkbox: (_$) => token(seq('[~]')),
     obsolete_task: ($) => seq($.obsolete_checkbox, optional(seq(' ', optional($.description)))),
-    description: ($) => choice(
-      $.main_line,
-      seq($.main_line, '\n', repeat(seq($.indent, '\n')), $.indent),
-    ),
+    description: ($) => choice($.main_line, seq($.main_line, '\n', repeat(seq($.indent, '\n')), $.indent)),
   },
 });
